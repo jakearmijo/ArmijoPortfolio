7b8922f6145e103ff5ae75333b3a27619e80065d
Jake Armijo <armijojake@yahoo.com>
2020-11-13 08:24:51 -0600

    Somewhat of a working styled-comp-carousel

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Carousel from 'styled-components-carousel'
 
 export default function Projects() {
   return (
@@ -12,7 +13,22 @@ export default function Projects() {
       </div>
       <div id="phutbol" data-aos="fade-up" className="aos-init aos-animate">
         <h1 className="projectTitle">Phutbol</h1>
-        <div></div>
+        <div>
+          <Carousel center infinite showArrows showIndicator slidesToShow={3}>
+            <div>
+              <span>
+                <img src="phutBol-loading-screen.png" />
+                Log In Screen
+              </span>
+            </div>
+            <div>
+              <span>2</span>
+            </div>
+            <div>
+              <span>3</span>
+            </div>
+          </Carousel>
+        </div>
       </div>
     </div>
   )
