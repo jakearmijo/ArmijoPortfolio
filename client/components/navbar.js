80b7b8403e63ecbcb333df6cbc388d37beb0d0df
Jake Armijo <armijojake@yahoo.com>
2020-11-22 16:20:08 -0600

    Implemented Scroll on Navbar click via react-scroll

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Link} from 'react-scroll'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
@@ -11,25 +12,41 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div className="nav-links">
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
+          {/* <a href="#" onClick={handleClick}>
             Logout
-          </a>
-          <Link to="/projects">PORTFOLIO</Link>
-          <Link to="/about">ABOUT</Link>
-          <Link to="/resume">RESUME</Link>
-          <Link to="/contact">CONTACT</Link>
+          </a> */}
+          <Link to="projects" activeClass="active" spy={true} smooth={true}>
+            PORTFOLIO
+          </Link>
+          <Link to="aboutMe" activeClass="active" spy={true} smooth={true}>
+            ABOUT
+          </Link>
+          <Link to="resume" activeClass="active" spy={true} smooth={true}>
+            RESUME
+          </Link>
+          <Link to="contactTitle" activeClass="active" spy={true} smooth={true}>
+            CONTACT
+          </Link>
           <Link to="https://github.com/jakearmijo">GITHUB</Link>
           <Link to="https://www.linkedin.com/in/jake-armijo/">LINKEDIN</Link>
         </div>
       ) : (
         <div className="nav-links">
           {/* The navbar will show these links before you log in */}
-          <Link to="/login">LOGIN</Link>
-          <Link to="/signup">SIGN UP</Link>
-          <Link to="/projects">PORTFOLIO</Link>
-          <Link to="/about">ABOUT</Link>
-          <Link to="/resume">RESUME</Link>
-          <Link to="/contact">CONTACT</Link>
+          {/* <Link to="/login">LOGIN</Link>
+          <Link to="/signup">SIGN UP</Link> */}
+          <Link to="projects" activeClass="active" spy={true} smooth={true}>
+            PORTFOLIO
+          </Link>
+          <Link to="aboutMe" activeClass="active" spy={true} smooth={true}>
+            ABOUT
+          </Link>
+          <Link to="resume" activeClass="active" spy={true} smooth={true}>
+            RESUME
+          </Link>
+          <Link to="contactTitle" activeClass="active" spy={true} smooth={true}>
+            CONTACT
+          </Link>
           <a href="https://github.com/jakearmijo">GITHUB</a>
           <a href="https://www.linkedin.com/in/jake-armijo/">LINKEDIN</a>
         </div>
